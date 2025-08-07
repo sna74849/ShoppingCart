a7fd550795797d3eed04154bc679d0efa684015b
sna74849 <sna74849@nifty.com>
2025-08-07 15:59:53 +0900

    一時的なコミット：作業中の変更を保存

--- Models/Entity/DestinationEntity.cs
+++ Models/Entity/DestinationEntity.cs
@@ -1,6 +1,5 @@
 ﻿namespace ShoppingCart.Models.Entity {
     public class DestinationEntity {
-<<<<<<< HEAD
         public int DestinationNo { get; set; } = default!;
         public string CustomerId { get; set; } = default!;
         public string Name { get; set; } = default!;
@@ -9,14 +8,5 @@
         public string Phone { get; set; } = default!;
         public DateTime CreatedAt { get; set; } = default!;
         public DateTime UpdatedAt { get; set; } = default!;
-=======
-        public int DestinationNo { get; set; }
-        public int CustomerId { get; set; }
-        public string Name { get; set; }
-        public string Postcode { get; set; }
-        public string Address { get; set; }
-        public string Phone { get; set; }
-        public string Email { get; set; }
->>>>>>> 91decb47f3a5019fbd405ce4ab88d86b6396da4d
     }
 }
